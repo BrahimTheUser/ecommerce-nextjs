fb874678fb45f1fb0fab08eeb9bcf47ba18a519c
Aouadi Brahim <aouadib86@gmail.com>
2025-07-22 21:40:39 +0100

    adding shadcn ui and configuring the theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Home</h1>
+      <h1 className="text-3xl font-bold underline font-barlow">Home 2</h1>
+      <Button variant="destructive">Click here</Button>
     </>
   );
 }
