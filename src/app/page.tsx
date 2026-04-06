1c889ed14ac78aa3955d2454b9f2d9a1f7b8ebd9
Aouadi Brahim <aouadib86@gmail.com>
2026-04-07 00:09:06 +0100

    feat: add offer tag management with CRUD operations, including UI components and database queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import ThemeToggle from "@/components/shared/theme-toggle";
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Home</h1>
-      <h1 className="text-3xl font-bold underline font-barlow">Home Page</h1>
-      <ThemeToggle />
-      <div className="grid place-content-center">
-        <UserButton />
-      </div>
-    </>
+    <div>
+      <UserButton />
+    </div>
   );
 }
